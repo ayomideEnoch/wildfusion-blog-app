0972bdc70a793eba152a47e5a64bb2bc02a541e4
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2022-03-29 09:52:04 +0100

    CSS BG Fix

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <a class="signature" href="https://ayomideibosiola.netlify.app" target="_blank">
-        <img src="../assets/vue-snip.svg"/>
+        <img src="../assets/wild-fusion.png"/>
       </a>
     </footer>
   </transition>
